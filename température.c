8b83c1955a3be61985d499fdd1c8736853bfcbaf
Achnit Reda <achnitreda@gmail.com>
2021-11-10 14:59:27 +0100

    temperature

---
+++ température.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float celsius, fahrenheit;
+    
+    printf("température en degré fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    
+    celsius = (fahrenheit - 32) * 5/9;
+
+    printf("%.2f", celsius);
+
+    return 0;
+    
+
+}

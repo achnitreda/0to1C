ae1d9afecc67402b951399be57e97c8ad79dbdf5
Achnit Reda <achnitreda@gmail.com>
2021-11-10 11:59:08 +0100

    projet final

---
+++ info.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int age,NR;
+    char nom[50];
+    char prenom[50];
+    char sexe[50];
+
+    printf("votre nom :");
+    scanf("%s", &nom);
+    printf("votre prenom :");
+    scanf("%s", &prenom);
+    printf("votre sexe :");
+    scanf("%s", &sexe);
+    printf("votre age :");
+    scanf("%d", &age);
+    printf("votre NR :");
+    scanf("%d", &NR);
+    
+    return 0;
+  
+}

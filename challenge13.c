a6dfc25d56367331e9eab8d2414213bbde00c7ef
Achnit Reda <achnitreda@gmail.com>
2021-11-11 14:31:37 +0100

    c

---
+++ challenge13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int nb;
+    
+    printf("entrez nombre:\n");
+    scanf("%d", &nb);
+
+    
+
+    printf("Hexadecimal : %x \n", &nb);
+    printf("octale : %o ", &nb);
+
+    return 0;
+
+
+
+}

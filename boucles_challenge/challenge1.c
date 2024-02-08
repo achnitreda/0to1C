a4b94794415a81b18fe7f9d0b9803dd6cf1ac9a1
achnitreda <achnitreda@gmail.com>
2024-02-08 19:26:12 +0100

    test

--- boucles_challenge/challenge1.c
+++ boucles_challenge/challenge1.c
@@ -5,11 +5,11 @@ int main()
 {
     int i;
     float N;
-    printf("enter a number:");
+    printf("Enter a Number:");
     scanf("%f",&N);
     
     for(i=0; i<=10; i++){
-      printf("%.2f × %d = %.2f \n",N ,i ,N*i);
+      printf("%.2f * %d = %.2f \n",N ,i ,N*i);
     
     }
     return 0;
